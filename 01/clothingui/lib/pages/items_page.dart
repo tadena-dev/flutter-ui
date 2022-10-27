22c7da5c4c4683bbd7e525d25e8662c031e16f99
Emmanuel Tadena Jr <tadena.dev@gmail.com>
2022-10-27 18:45:05 +0800

    2nd UI commit

--- 01/clothingui/lib/pages/items_page.dart
+++ 01/clothingui/lib/pages/items_page.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
 
-class ItemsPage extends StatelessWidget {
+class ItemsPage extends StatefulWidget {
   const ItemsPage({super.key});
 
   @override
+  State<ItemsPage> createState() => _ItemsPageState();
+}
+
+class _ItemsPageState extends State<ItemsPage> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Top Sales'),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.shopping_bag_rounded),
+            ),
+          ],
+          bottom: const TabBar(
+            tabs: [
+              Icon(Icons.male),
+              Icon(Icons.female),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: const [
+            Center(
+              child: Text('hello'),
+            ),
+            Center(
+              child: Text('hello'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
